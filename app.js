6a7607778534406a7b0e6c13fef5a6385c755296
Felipe <feliperosenek@gmail.com>
2024-11-24 16:40:13 -0300

    deploy

--- app.js
+++ app.js
@@ -455,6 +455,6 @@ app.post('/create-task', async (req, res) => {
 
 
 // Inicia o servidor
-server.listen(4075, () => {
+server.listen(process.env.PORT || 4075, () => {
   console.log('Servidor rodando na porta 4075');
 });
