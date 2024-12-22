61667bf94af4e309fa1ef883d804bb5826cd6815
Felipe <feliperosenek@gmail.com>
2024-12-22 14:58:45 -0300

    firebase, serviceAccountWindows

---
+++ firebaseAdmin.js
@@ -0,0 +1,10 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json'); // Substitua pelo caminho correto
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const messaging = admin.messaging();
+
+module.exports = { messaging };
